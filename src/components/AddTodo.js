f8ebba8be3a77c23fd038d023dfec7c81f336e60
João Pedro <joaopedrosanchez302@gmail.com>
2020-11-27 16:01:30 -0300

    Deleted unused vars

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
